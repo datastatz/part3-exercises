6407ac29b10b1deee19ccdb210fbab9c1ad89e98
datastatz <155395474+datastatz@users.noreply.github.com>
2025-02-15 16:09:15 +0100

    Finished exercise 1

--- index.js
+++ index.js
@@ -4,10 +4,39 @@ const app = express();
 // For parsing JSON bodies in requests:
 app.use(express.json());
 
-// Example route:
-app.get('/', (req, res) => {
-  res.send('Hello, Express!');
+
+// All the data of the PhoneBook
+const data = [
+  { 
+    "id": "1",
+    "name": "Arto Hellas", 
+    "number": "040-123456"
+  },
+  { 
+    "id": "2",
+    "name": "Ada Lovelace", 
+    "number": "39-44-5323523"
+  },
+  { 
+    "id": "3",
+    "name": "Dan Abramov", 
+    "number": "12-43-234345"
+  },
+  { 
+    "id": "4",
+    "name": "Mary Poppendieck", 
+    "number": "39-23-6423122"
+  }
+]
+
+
+// GET request route
+app.get('/api/persons', (req, res) => {
+  res.json(data);
 });
+
+
+
 
 // Start the server:
 const PORT = 3000;
