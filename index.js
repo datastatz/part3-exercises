fa3547544ce146cd13461e8229cead0576342e34
datastatz <155395474+datastatz@users.noreply.github.com>
2025-02-17 16:09:58 +0100

    test

--- index.js
+++ index.js
@@ -121,7 +121,7 @@ app.post("/api/persons", (req, res) => {
 
 
 // Start the server:
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
